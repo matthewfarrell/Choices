04859dfe47432cde0fafdc1d49a5ad70d963b031
Josh Johnson <josh@joshuajohnson.co.uk>
2018-03-28 13:02:49 +0100

    Add further tests

--- src/scripts/src/templates.test.js
+++ src/scripts/src/templates.test.js
@@ -5,9 +5,143 @@ import { getType, strToEl } from './lib/utils';
 const stripElement = element => element.outerHTML.replace(/(^|>)\s+|\s+(?=<|$)/g, '$1');
 
 describe('templates', () => {
-  // describe('containerOuter', () => {
+  describe('containerOuter', () => {
+    const classes = {
+      containerOuter: 'test',
+    };
+    const direction = 'rtl';
 
-  // });
+    describe('select element', () => {
+      describe('search enabled', () => {
+        it('returns expected html', () => {
+          const isSelectElement = true;
+          const isSelectOneElement = false;
+          const searchEnabled = true;
+          const passedElementType = 'select-multiple';
+
+          const expectedOutput = strToEl(`
+            <div
+              class="${classes.containerOuter}"
+              data-type="${passedElementType}"
+              role="combobox"
+              aria-autocomplete="list"
+              aria-haspopup="true"
+              aria-expanded="false"
+              dir="${direction}"
+              >
+            </div>
+          `);
+          const actualOutput = templates.containerOuter(
+            classes,
+            direction,
+            isSelectElement,
+            isSelectOneElement,
+            searchEnabled,
+            passedElementType,
+          );
+
+          expect(getType(actualOutput)).to.equal('HTMLDivElement');
+          expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+        });
+      });
+
+      describe('search disabled', () => {
+        it('returns expected html', () => {
+          const isSelectElement = true;
+          const isSelectOneElement = false;
+          const searchEnabled = false;
+          const passedElementType = 'select-multiple';
+
+          const expectedOutput = strToEl(`
+            <div
+              class="${classes.containerOuter}"
+              data-type="${passedElementType}"
+              role="listbox"
+              aria-haspopup="true"
+              aria-expanded="false"
+              dir="${direction}"
+              >
+            </div>
+          `);
+          const actualOutput = templates.containerOuter(
+            classes,
+            direction,
+            isSelectElement,
+            isSelectOneElement,
+            searchEnabled,
+            passedElementType,
+          );
+
+          expect(getType(actualOutput)).to.equal('HTMLDivElement');
+          expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+        });
+      });
+
+      describe('select one element', () => {
+        it('returns expected html', () => {
+          const isSelectElement = true;
+          const isSelectOneElement = true;
+          const searchEnabled = false;
+          const passedElementType = 'select-one';
+
+          const expectedOutput = strToEl(`
+            <div
+              class="${classes.containerOuter}"
+              data-type="${passedElementType}"
+              role="listbox"
+              tabindex="0"
+              aria-haspopup="true"
+              aria-expanded="false"
+              dir="${direction}"
+              >
+            </div>
+          `);
+          const actualOutput = templates.containerOuter(
+            classes,
+            direction,
+            isSelectElement,
+            isSelectOneElement,
+            searchEnabled,
+            passedElementType,
+          );
+
+          expect(getType(actualOutput)).to.equal('HTMLDivElement');
+          expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+        });
+      });
+    });
+
+    describe('non select element', () => {
+      it('returns expected html', () => {
+        const isSelectElement = false;
+        const isSelectOneElement = false;
+        const searchEnabled = false;
+        const passedElementType = 'text';
+
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.containerOuter}"
+            data-type="${passedElementType}"
+            aria-haspopup="true"
+            aria-expanded="false"
+            dir="${direction}"
+            >
+          </div>
+        `);
+        const actualOutput = templates.containerOuter(
+          classes,
+          direction,
+          isSelectElement,
+          isSelectOneElement,
+          searchEnabled,
+          passedElementType,
+        );
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+  });
 
   describe('containerInner', () => {
     it('returns expected html', () => {
@@ -23,13 +157,14 @@ describe('templates', () => {
   });
 
   describe('itemList', () => {
+    const classes = {
+      list: 'test 1',
+      listSingle: 'test 2',
+      listItems: 'test 3',
+    };
+
     describe('select one element', () => {
       it('returns expected html', () => {
-        const classes = {
-          list: 'test 1',
-          listSingle: 'test 2',
-          listItems: 'test 3',
-        };
         const expectedOutput = strToEl(`<div class="${classes.list} ${classes.listSingle}"></div>`);
         const actualOutput = templates.itemList(classes, true);
 
@@ -40,11 +175,6 @@ describe('templates', () => {
 
     describe('non select one element', () => {
       it('returns expected html', () => {
-        const classes = {
-          list: 'test 1',
-          listSingle: 'test 2',
-          listItems: 'test 3',
-        };
         const expectedOutput = strToEl(`<div class="${classes.list} ${classes.listItems}"></div>`);
         const actualOutput = templates.itemList(classes, false);
 
@@ -59,7 +189,7 @@ describe('templates', () => {
       const classes = {
         placeholder: 'test',
       };
-      const value = 'Test';
+      const value = 'test';
       const expectedOutput = strToEl(`
         <div class="${classes.placeholder}">${value}</div>`,
       );
@@ -75,11 +205,12 @@ describe('templates', () => {
   // });
 
   describe('choiceList', () => {
+    const classes = {
+      list: 'test',
+    };
+
     describe('select one element', () => {
       it('returns expected html', () => {
-        const classes = {
-          list: 'test',
-        };
         const expectedOutput = strToEl(`
           <div
             class="${classes.list}"
@@ -97,9 +228,6 @@ describe('templates', () => {
 
     describe('non select one element', () => {
       it('returns expected html', () => {
-        const classes = {
-          list: 'test',
-        };
         const expectedOutput = strToEl(`
           <div
             class="${classes.list}"
@@ -117,20 +245,218 @@ describe('templates', () => {
     });
   });
 
-  // describe('choiceGroup', () => {
+  describe('choiceGroup', () => {
+    const classes = {
+      group: 'test 1',
+      groupHeading: 'test 2',
+      itemDisabled: 'test 3',
+    };
 
-  // });
+    let data;
 
-  // describe('choice', () => {
+    beforeEach(() => {
+      data = {
+        id: 1,
+        value: 'test',
+        disabled: false,
+      };
+    });
 
-  // });
+    describe('enabled state', () => {
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+          class="${classes.group}"
+            data-group
+            data-id="${data.id}"
+            data-value="${data.value}"
+            role="group"
+            >
+            <div class="${classes.groupHeading}">${data.value}</div>
+          </div>
+        `);
+        const actualOutput = templates.choiceGroup(classes, data);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+
+    describe('disabled state', () => {
+      beforeEach(() => {
+        data = {
+          ...data,
+          disabled: true,
+        };
+      });
+
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.group} ${classes.itemDisabled}"
+            data-group
+            data-id="${data.id}"
+            data-value="${data.value}"
+            role="group"
+            aria-disabled="true"
+            >
+            <div class="${classes.groupHeading}">${data.value}</div>
+          </div>
+        `);
+        const actualOutput = templates.choiceGroup(classes, data);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+  });
+
+  describe('choice', () => {
+    const classes = {
+      item: 'test 1',
+      itemChoice: 'test 2',
+      itemDisabled: 'test 3',
+      itemSelectable: 'test 4',
+      placeholder: 'test 5',
+    };
+
+    const itemSelectText = 'test 6';
+
+    let data;
+
+    beforeEach(() => {
+      data = {
+        id: 1,
+        groupId: -1,
+        disabled: false,
+        elementId: 'test',
+        label: 'test',
+        value: 'test',
+      };
+    });
+
+    describe('enabled state', () => {
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.item} ${classes.itemChoice} ${classes.itemSelectable}"
+            data-select-text="${itemSelectText}"
+            data-choice
+            data-id="${data.id}"
+            data-value="${data.value}"
+            data-choice-selectable
+            id="${data.elementId}"
+            role="option"
+            >
+            ${data.label}
+          </div>
+        `);
+        const actualOutput = templates.choice(classes, data, itemSelectText);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+
+    describe('disabled state', () => {
+      beforeEach(() => {
+        data = {
+          ...data,
+          disabled: true,
+        };
+      });
+
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.item} ${classes.itemChoice} ${classes.itemDisabled}"
+            data-select-text="${itemSelectText}"
+            data-choice
+            data-id="${data.id}"
+            data-value="${data.value}"
+            data-choice-disabled
+            aria-disabled="true"
+            id="${data.elementId}"
+            role="option"
+            >
+            ${data.label}
+          </div>
+        `);
+        const actualOutput = templates.choice(classes, data, itemSelectText);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+
+    describe('placeholder', () => {
+      beforeEach(() => {
+        data = {
+          ...data,
+          placeholder: true,
+        };
+      });
+
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.item} ${classes.itemChoice} ${classes.itemSelectable} ${classes.placeholder}"
+            data-select-text="${itemSelectText}"
+            data-choice
+            data-id="${data.id}"
+            data-value="${data.value}"
+            data-choice-selectable
+            id="${data.elementId}"
+            role="option"
+            >
+            ${data.label}
+          </div>
+        `);
+        const actualOutput = templates.choice(classes, data, itemSelectText);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+
+    describe('child of group', () => {
+      beforeEach(() => {
+        data = {
+          ...data,
+          groupId: 1
+        };
+      });
+
+      it('returns expected html', () => {
+        const expectedOutput = strToEl(`
+          <div
+            class="${classes.item} ${classes.itemChoice} ${classes.itemSelectable}"
+            data-select-text="${itemSelectText}"
+            data-choice
+            data-id="${data.id}"
+            data-value="${data.value}"
+            data-choice-selectable
+            id="${data.elementId}"
+            role="treeitem"
+            >
+            ${data.label}
+          </div>
+        `);
+        const actualOutput = templates.choice(classes, data, itemSelectText);
+
+        expect(getType(actualOutput)).to.equal('HTMLDivElement');
+        expect(stripElement(actualOutput)).to.equal(stripElement(expectedOutput));
+      });
+    });
+  });
 
   describe('input', () => {
+    const classes = {
+      input: 'test 1',
+      inputCloned: 'test 2',
+    };
+
     it('returns expected html', () => {
-      const classes = {
-        input: 'Test 1',
-        inputCloned: 'Test 2',
-      };
       const expectedOutput = strToEl(`
         <input
           type="text"
@@ -150,12 +476,13 @@ describe('templates', () => {
   });
 
   describe('dropdown', () => {
+    const classes = {
+      list: 'test 1',
+      listDropdown: 'test 2',
+    }
+    ;
     it('returns expected html', () => {
-      const classes = {
-        list: 'test 1',
-        listDropdown: 'test 2',
-      };
-      const value = 'Test';
+      const value = 'test';
       const expectedOutput = strToEl(`<div class="${classes.list} ${classes.listDropdown}" aria-expanded="false"></div>`);
       const actualOutput = templates.dropdown(classes, value);
 
@@ -165,14 +492,16 @@ describe('templates', () => {
   });
 
   describe('notice', () => {
+    const classes = {
+      item: 'test 1',
+      itemChoice: 'test 2',
+      noResults: 'test 3',
+      noChoices: 'test 4',
+    };
+
+    const label = 'test';
+
     it('returns expected html', () => {
-      const classes = {
-        item: 'test 1',
-        itemChoice: 'test 2',
-        noResults: 'test 3',
-        noChoices: 'test 4',
-      };
-      const label = 'Test';
       const expectedOutput = strToEl(`
         <div class="${classes.item} ${classes.itemChoice}">
           ${label}
@@ -187,13 +516,6 @@ describe('templates', () => {
     describe('passing a notice type', () => {
       describe('no results', () => {
         it('adds no results classname', () => {
-          const classes = {
-            item: 'test 1',
-            itemChoice: 'test 2',
-            noResults: 'test 3',
-            noChoices: 'test 4',
-          };
-          const label = 'Test';
           const expectedOutput = strToEl(`
             <div class="${classes.item} ${classes.itemChoice} ${classes.noResults}">
               ${label}
@@ -207,13 +529,6 @@ describe('templates', () => {
 
       describe('no choices', () => {
         it('adds no choices classname', () => {
-          const classes = {
-            item: 'test 1',
-            itemChoice: 'test 2',
-            noResults: 'test 3',
-            noChoices: 'test 4',
-          };
-          const label = 'Test';
           const expectedOutput = strToEl(`
             <div class="${classes.item} ${classes.itemChoice} ${classes.noChoices}">
               ${label}
@@ -228,13 +543,18 @@ describe('templates', () => {
   });
 
   describe('option', () => {
-    it('returns expected html', () => {
-      const data = {
+    let data;
+
+    beforeEach(() => {
+      data = {
         disabled: false,
         selected: false,
-        value: 'Test value',
-        label: 'Test label',
+        value: 'test value',
+        label: 'test label',
       };
+    });
+
+    it('returns expected html', () => {
       const expectedOutput = strToEl(`<option value="${data.value}" ${data.selected ? 'selected' : ''} ${data.disabled ? 'disabled' : ''}>${data.label}</option>`);
       const actualOutput = templates.option(data);
 
@@ -243,29 +563,29 @@ describe('templates', () => {
     });
 
     describe('when selected', () => {
-      it('sets selected attr to true', () => {
-        const data = {
-          disabled: false,
+      beforeEach(() => {
+        data = {
+          ...data,
           selected: true,
-          value: 'Test value',
-          label: 'Test label',
         };
-        const output = templates.option(data);
+      });
 
+      it('sets selected attr to true', () => {
+        const output = templates.option(data);
         expect(output.selected).to.equal(true);
       });
     });
 
     describe('when disabled', () => {
-      it('sets disabled attr to true', () => {
-        const data = {
+      beforeEach(() => {
+        data = {
+          ...data,
           disabled: true,
-          selected: false,
-          value: 'Test value',
-          label: 'Test label',
         };
-        const output = templates.option(data);
+      });
 
+      it('sets disabled attr to true', () => {
+        const output = templates.option(data);
         expect(output.disabled).to.equal(true);
       });
     });
